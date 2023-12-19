189f5b3f325ea34d5a667b010c5143fd4b0bd89d
alfinrz <alfinrq@gmail.com>
2023-12-19 09:59:49 +0700

    CVM training model

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.utils.data as Data
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 from metrics import *
 from ped_dataset import *
 
@@ -112,6 +116,62 @@ def parse_commandline():
     parser.add_argument('--sample', default=RunConfig.sample, action='store_true', help='Turns on the sampling for the CVM (OUR-S).')
     args = parser.parse_args()
     return args
+
+
+class ConstantVelocityModel(nn.Module):
+    def __init__(self):
+        super(ConstantVelocityModel, self).__init__()
+
+    def forward(self, observed):
+        obs_rel = observed[:, 1:] - observed[:, :-1]
+        deltas = obs_rel[:, -1].unsqueeze(1)
+        y_pred_rel = deltas.repeat(1, 12, 1)
+        return y_pred_rel
+
+def train_cvm_model(model, train_loader, criterion, optimizer, num_epochs=10):
+    model.train()
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for batch_x, batch_y in train_loader:
+            optimizer.zero_grad()
+
+            observed = batch_x.permute(1, 0, 2)
+            y_true_rel, masks = batch_y
+            y_true_rel = y_true_rel.permute(1, 0, 2)
+
+            y_pred_rel = model(observed)
+            loss = criterion(y_pred_rel, y_true_rel)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        avg_loss = running_loss / len(train_loader)
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss}")
+
+# Use this function to create a DataLoader for your training dataset
+def create_train_loader(trainset, batch_size):
+    return torch.utils.data.DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True)
+
+def main():
+    # Load datasets
+    datasets = load_datasets()
+
+    # Choose a dataset for training (modify this part as needed)
+    train_dataset = datasets[0]
+    train_loader = create_train_loader(train_dataset, batch_size=1)
+
+    # Initialize the CVM model, criterion, and optimizer
+    cvm_model = ConstantVelocityModel()
+    criterion = nn.MSELoss()  # You may need to choose an appropriate loss function
+    optimizer = optim.Adam(cvm_model.parameters(), lr=0.001)  # Adjust the learning rate as needed
+
+    # Train the CVM model
+    train_cvm_model(cvm_model, train_loader, criterion, optimizer, num_epochs=10)
+
+    # Now you can use the trained model for evaluation or other tasks
 
 def plot_training_history(train_loss, train_acc, val_loss, val_acc):
     epochs = range(1, len(train_loss) + 1)
